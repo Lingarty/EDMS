0b1e50237e4cc45e441be102a173a1b593df6d78
Lingarty <lingarty@gmail.com>
2019-01-23 12:44:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,12 +16,12 @@ namespace test
         {
             Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHldx37RiP4Tx0cm572DgkZJjTH54tkuWQ0Er1ZaUyqFzoFIIo" +
                 "EmHoalDuXRCdiRYT9GiVcZmqQ10v4+yT0fSlbbyjluyXq2aLLJ5tULO5kCwECd/i2DN8GnN4Azl8LXUZlOVS/qFhQB" +
-                "Qs59ZhSta/VbwCniUNRUxssfp2x8lIcaiYS32/AEDiuWQdTid3MV/fH9oA7cZDtUAX548z4oCcNWRZhFsWm9wrrM0k" +
-                "iF/uVbzwKC+6ZpVarbs63N9xV4YR5a7LNDd+Rnu7M0Y9NvIvt/+re+VUG9weRabxN8cUTmFQHX1Ap0ZyfXr853lfOR" +
+                "Qs59ZhSta/VbwCniUNRUxssfp2x8lIcaiYS32/AEDiuWQdTid3MV/fH9оA7cZDtUAX548z4oCcNWRZhFsWm9wrrM0k" +
+                "iF/uVbzwKC+6ZpVarbs63N9xV4YR5a7LNDd+Rnu7M0Y9NvIvt/+re+VUG9wеRabxN8сUTmFQHX1Ap0ZyfXr853lfOR" +
                 "Q9VPlRo0O6BM9xYVGj3UJJ537nPDWeoRdTIRddPwrFIXBBKKFBgQm1v+rp71JT/guWfedjqRt9vEe3IsfV5MCF5KkC" +
                 "NfdySR3V6Q65lWVPgHFwNjYhhJrm7Ohn6xnkjkjINhlDsj83TEtSz0PqbRfPetxQIsmrmhKSVINTk+JWyp+ylblnps" +
                 "Izu09tvAJWan9rlHLoaHI9K9T28M7W2zR0KgtCMNNp8p8OULCiDYYTufG30jtJpaMhfWpTAL+LObgvF0g9bKV7Sg9+" +
-                "B8OyXchVmrIe6I6IVM5/mYaqj6+9VIXE7XzU5Sikq4zqYOUnfbh6E7sjni+/vaLsXZgg+C++PxkFUUI3OhSbc8eDk9" +
+                "B8OyXchVmrIe6I6IVM5/mYаqj6+9VIXE7XzU5Sikq4zqYOUnfbh6E7sjni+/vaLsXZgg+C++PxkFUUI3OhSbc8еDk9" +
                 "SG8z5p4ePZxVrme/72ILtpgcpms=";
             BuildWebHost(args).Run();
         }
